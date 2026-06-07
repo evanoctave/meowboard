71f93da659bbe387c410fcdf21fd5dc0cbbbc25c
evanoctave <evanoctav3@gmail.com>
2026-06-06 21:05:33 -0700

    Initial Commit

---
+++ Meowboard/MeowboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeowboardApp.swift
+//  Meowboard
+//
+//  Created by Evan B on 6/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MeowboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
